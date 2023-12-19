cc4015ed7bd2911ea74638e9b1c43ec6538fc0fe
Sathyam Sahu <sathyamrock55@gmail.com>
2023-12-19 18:03:17 +0530

    Done

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,21 @@
 */
 
 function isAnagram(str1, str2) {
+    // Remove all the spaces and convert all the letters to lower case.
+    const cleanStr1 = str1.replace(/\s/g, '').toLowerCase();
+    const cleanStr2 = str2.replace(/\s/g, '').toLowerCase();
 
+    // Check if the lengths are different, if yes, they can't be anagrams
+    if (cleanStr1.length !== cleanStr2.length) {
+        return false;
+    }
+
+    // Sort the characters in both strings and compare
+    const sortedStr1 = cleanStr1.split('').sort().join('');
+    const sortedStr2 = cleanStr2.split('').sort().join('');
+
+    // Check if the sorted strings are equal
+    return sortedStr1 === sortedStr2;
 }
 
 module.exports = isAnagram;
